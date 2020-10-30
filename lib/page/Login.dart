05d28992710a600ae483b3658f3cf1df6ac0a878
Warisara C <warisara@Warisaras-MacBook-Pro.local>
2020-10-30 16:08:23 +0700

    initial commit

---
+++ lib/page/Login.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFC37447),
+        title: Text('AbdoCare'),
+      ),
+      body: Container(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 200, 50, 0),
+              child: TextField(
+                //ทำให้textไปตรงกลางของช่อง
+                textAlign: TextAlign.center,
+                //ใส่ว่าช่องนี้จะใส่อะไรเป็นข้อความแนะนำ
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'username',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: TextField(
+                //Hide code
+                obscureText: true,
+                //ทำให้textไปตรงกลางของช่อง
+                textAlign: TextAlign.center,
+                //ใส่ว่าช่องนี้จะใส่อะไรเป็นข้อความแนะนำ
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'password',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 10, 50, 150),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  RaisedButton(
+                    textColor: Colors.white,
+                    color: Color(0xFF33cc33),
+                    child: Text('เข้าสู่ระบบ', style: TextStyle(fontSize: 18)),
+                    onPressed: () {
+                      print('This is login button');
+                      setState(() {});
+                      Navigator.pushNamed(context, '/PostHos_page');
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/Hospital_logo.png',
+                      height: 100,
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/STIN_logo.png',
+                      height: 100,
+                      width: 80,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
