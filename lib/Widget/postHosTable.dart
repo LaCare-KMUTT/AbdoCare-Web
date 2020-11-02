b319d93de457170c365f6c8a66104f4ed48d7c4b
Warisara C <warisara@Warisaras-MacBook-Pro.local>
2020-11-02 16:00:58 +0700

    Add post op page

---
+++ lib/Widget/postHosTable.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class PostHosTable extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PostHosTableState();
+  }
+}
+
+class _PostHosTableState extends State<PostHosTable> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        SizedBox(
+          height: 50,
+        ),
+        Center(
+          child: Text(
+            'Post-op @ Hospital',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ]),
+    );
+  }
+}
