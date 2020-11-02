9980d9a9c6ad9a0914d7f0483e36266142e43c81
Warisara C <warisara@Warisaras-MacBook-Pro.local>
2020-11-02 14:14:37 +0700

    change drawer in post-hospital page

--- lib/page/PostHos.dart
+++ lib/page/PostHos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:AbdoCare_Web/Widget/sidebar.dart';
 
 class PostHosPage extends StatefulWidget {
   @override
@@ -27,65 +28,7 @@ class _PostHosPageState extends State<PostHosPage> {
           ],
         ),
       ),
-      drawer: Drawer(
-        // child: Container(
-        //   color: Color(0xFFC37447),
-        child: LayoutBuilder(
-          builder: (context, constraint) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraint.maxHeight),
-                child: IntrinsicHeight(
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(
-                          Icons.account_circle_rounded,
-                        ),
-                        title: Text('เพิ่มผู้ป่วยใหม่'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/addPatient_page');
-                        },
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.pending_actions),
-                        title: Text('Pre-op'),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/Pre_page');
-                        },
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.local_hospital),
-                        title: Text("Post-op@Hospital"),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/PostHos_page');
-                        },
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.house),
-                        title: Text("Post-op@Community"),
-                        onTap: () {
-                          Navigator.pushNamed(context, '/PostCom_page');
-                        },
-                      ),
-                      const Expanded(child: SizedBox()),
-                      const Divider(height: 1.0, color: Colors.grey),
-                      ListTile(
-                        leading: Icon(Icons.exit_to_app),
-                        title: Text("ออกจากระบบ"),
-                        onTap: () {
-                          Navigator.pushReplacementNamed(context, '/');
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-          //),
-        ),
-      ),
+      drawer: SideBar(),
     );
   }
 }
