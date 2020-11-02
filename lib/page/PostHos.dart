0f8596ee32211d59e8131297d02ad1f725bb47f3
Warisara C <warisara@Warisaras-MacBook-Pro.local>
2020-11-02 16:02:47 +0700

    Add posthos to body

--- lib/page/PostHos.dart
+++ lib/page/PostHos.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:AbdoCare_Web/Widget/sidebar.dart';
 
+import '../Widget/postHosTable.dart';
+import '../Widget/sidebar.dart';
+
 class PostHosPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -16,18 +19,19 @@ class _PostHosPageState extends State<PostHosPage> {
         title: const Text('AbdoCare'),
         backgroundColor: Color(0xFFC37447),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Post-op @ Hospital',
-              style: TextStyle(fontSize: 18),
-            )
-          ],
-        ),
-      ),
+      body: PostHosTable(),
+      // body: Padding(
+      //   padding: const EdgeInsets.only(top: 16),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text(
+      //         'Post-op @ Hospital',
+      //         style: TextStyle(fontSize: 18),
+      //       )
+      //     ],
+      //   ),
+      // ),
       drawer: SideBar(),
     );
   }
