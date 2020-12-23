a47aef45c925176d65d43564426ee36734610484
Thaweesak Saiwongse <Thaweesak.Saiwongse@agoda.com>
2020-12-23 18:07:01 +0700

    wip

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'interfaces/firebase_service_interface.dart';
@@ -16,6 +17,16 @@ class FirebaseService extends IFirebaseService {
     @required String collection,
     @required String docId,
     @required Map<String, dynamic> data,
-  }) {}
+  }) async {
+    FirebaseApp tempApp = await Firebase.initializeApp(
+        name: 'Temporary Register', options: Firebase.app().options);
+    UserCredential tempAuthResult = await FirebaseAuth.instanceFor(app: tempApp)
+        .createUserWithEmailAndPassword(
+            email: data['username'], password: data['uniqueKey']);
+    var writeData = data.keys.toList();
+    print(writeData);
+    _firestore.collection('Users').doc(tempAuthResult.user.uid).set({});
+  }
+
   String getUserId() => _auth.currentUser.uid;
 }
