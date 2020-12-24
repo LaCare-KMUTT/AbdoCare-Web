9850ee7ec6cff93ab327395a0ae0ddcf9b7b20c7
Thaweesak Saiwongse <Thaweesak.Saiwongse@agoda.com>
2020-12-24 17:10:37 +0700

    add Test UI to create mock data

---
+++ lib/page/mockDataPage.dart
@@ -0,0 +1,38 @@
+import 'package:AbdoCare_Web/services/interfaces/firebase_service_interface.dart';
+import 'package:AbdoCare_Web/services/service_locator.dart';
+import 'package:flutter/material.dart';
+
+class MockDataPage extends StatelessWidget {
+  final IFirebaseService _firebaseService = locator<IFirebaseService>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFC37447),
+        title: Text('AbdoCare'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(50, 20, 50, 150),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7.0)),
+              textColor: Colors.white,
+              color: Color(0xFF2ED47A),
+              child:
+                  Text('สร้าง Patient Account', style: TextStyle(fontSize: 18)),
+              onPressed: () {
+                _firebaseService
+                    .createPatient(collection: "test", data: {"test": "test"});
+                print('สร้าง Patient Account');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
