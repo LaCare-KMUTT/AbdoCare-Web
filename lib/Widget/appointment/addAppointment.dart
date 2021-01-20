b62f9299151d6333688988db8f2ae3cf81c4cb47
prawproudd <prawproudd@hotmail.com>
2021-01-20 19:01:05 +0700

    material color widget

--- lib/Widget/appointment/addAppointment.dart
+++ lib/Widget/appointment/addAppointment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 
+import '../../Widget/material.dart';
 import '../../services/interfaces/calculation_service_interface.dart';
 import '../../services/service_locator.dart';
 
@@ -69,26 +70,6 @@ class _AddAppointmentPageState extends State<AddAppointmentPage> {
 
         print(_time);
       });
-  }
-
-  MaterialColor createMaterialColor(Color color) {
-    List strengths = <double>[.05];
-    Map swatch = <int, Color>{};
-    final int r = color.red, g = color.green, b = color.blue;
-
-    for (int i = 1; i < 10; i++) {
-      strengths.add(0.1 * i);
-    }
-    strengths.forEach((strength) {
-      final double ds = 0.5 - strength;
-      swatch[(strength * 1000).round()] = Color.fromRGBO(
-        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-        1,
-      );
-    });
-    return MaterialColor(color.value, swatch);
   }
 
   @override
