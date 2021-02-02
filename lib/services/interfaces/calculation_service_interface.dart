e81c86974975809ac45bff38d0dc94b6809f2648
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-02-02 16:53:47 +0700

    Provide Documentation for formatDateToThaiString

--- lib/services/interfaces/calculation_service_interface.dart
+++ lib/services/interfaces/calculation_service_interface.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 abstract class ICalculationService {
   int calculateAge({DateTime birthDate, String birthDateString});
   DateTime formatDate({DateTime date, String dateString});
+
+  /// If `date`/`dateString` is already a buddhist year. Set `isBuddhist = true`
   String formatDateToThaiString(
       {DateTime date, String dateString, @required bool isBuddhist});
 }
