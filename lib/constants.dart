0269355890cc7248ea0138c85641ec40a18873dc
Pormpimon <papangdroppaul@gmail.com>
2021-04-12 10:31:32 +0700

    make Constant file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class Constant {
+  static const btUpperCriteria = 37.0;
+  static const btLowerCriteria = 36.0;
+  static const prUpperCriteria = 100;
+  static const prLowerCriteria = 60;
+  static const rrUpperCriteria = 20;
+  static const rrLowerCriteria = 16;
+  static const systolicCriteria = 130;
+  static const diastolicCriteris = 80;
+}
