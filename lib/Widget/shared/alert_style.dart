26ef7863172c66d0f739aa6d691cbdfef2f78238
Pormpimon <papangdroppaul@gmail.com>
2021-04-14 23:15:45 +0700

    create alertdialog

--- lib/Widget/shared/alert_style.dart
+++ lib/Widget/shared/alert_style.dart
@@ -1,3 +1,5 @@
+import 'package:AbdoCare_Web/page/dashboard_postHos.dart';
+import 'package:AbdoCare_Web/page/dashboard_pre.dart';
 import 'package:flutter/material.dart';
 
 class Dialogs {
@@ -19,6 +21,40 @@ class Dialogs {
                 },
               )
             ]);
+      },
+    );
+  }
+
+  static Future<void> alertDialogSaveData(
+      BuildContext context, String hn, String patients) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        Future.delayed(Duration(seconds: 1), () {
+          print(patients);
+          if (patients == "Post-Operation@Hospital") {
+            return Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PostHosDashboardPage(hn: hn)));
+          } else if (patients == "Pre-Operation") {
+            return Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PreDashboardPage(hn: hn)),
+            );
+          }
+        });
+        return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            title: Icon(
+              Icons.check_circle_outline_rounded,
+              size: 100,
+              color: Colors.greenAccent,
+            ),
+            content: Text("บันทึกข้อมูลสำเร็จ",
+                style: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.center));
       },
     );
   }
