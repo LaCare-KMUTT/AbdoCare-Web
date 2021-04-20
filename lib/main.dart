a5d7be18ecf25b0b891502cbb557996b87b5b181
prawproudd <prawproudd@hotmail.com>
2021-04-20 18:38:41 +0700

    removed comment

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,17 @@ import 'page/pre.dart';
 import 'services/service_locator.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  // await Firebase.initializeApp(
-  //   name: 'Abdocare-Register-Service',
-  //   options: const FirebaseOptions(
-  //       projectId: 'abdocare-bdab0',
-  //       apiKey: 'AIzaSyDu8_HYCIsMBc8PkNs5oddKhjcJLFoNaVk',
-  //       authDomain: 'abdocare-bdab0.firebaseapp.com',
-  //       appId: '1:611491873650:web:7c3d372d1cdf8f314393eb',
-  //       messagingSenderId: '611491873650'),
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    name: 'Abdocare-Register-Service',
+    options: const FirebaseOptions(
+        projectId: 'abdocare-bdab0',
+        apiKey: 'AIzaSyDu8_HYCIsMBc8PkNs5oddKhjcJLFoNaVk',
+        authDomain: 'abdocare-bdab0.firebaseapp.com',
+        appId: '1:611491873650:web:7c3d372d1cdf8f314393eb',
+        messagingSenderId: '611491873650'),
+  );
   //ignore: await_only_futures
   await setupServiceLocator(isMock: false);
   runApp(MyApp());
