a8e46ba3b018190153a1914b2cdd086eb1443af0
Pormpimon <papangdroppaul@gmail.com>
2021-04-23 17:24:50 +0700

    set static alert

--- lib/Widget/shared/alert_style.dart
+++ lib/Widget/shared/alert_style.dart
@@ -58,4 +58,27 @@ class Dialogs {
       },
     );
   }
+
+  static Future<void> alertSuccessfullyChangeStatus(
+      BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            title: Text("ดำเนินการเรียบร้อย",
+                style: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.center),
+            actions: [
+              FlatButton(
+                child: Text('ตกลง'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ]);
+      },
+    );
+  }
 }
