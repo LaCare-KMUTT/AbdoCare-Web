62d73345a0c72a44001dc1b44f1569124f2c0484
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-05-03 04:13:45 +0700

    Use Cloud Function on register feature (#83)
    * use cloud function on register
    
    * add gitignore
    
    * error handling on createPatient
    
    * Move create user
    
    * move create user
    
    * try to write test
    
    * remove unused dependency

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as admin from "firebase-admin";
-import * as createUser from './createUser';
-
+import * as createUser from "./modules/createUser";
 
 admin.initializeApp();
 exports.createUser = createUser.createUser;
