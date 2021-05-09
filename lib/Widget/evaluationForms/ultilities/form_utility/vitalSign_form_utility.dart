244a9731da5ad7d02a7af902370da3a59e41fd95
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-09 14:26:05 +0700

    rename constant

--- lib/Widget/evaluationForms/ultilities/form_utility/vitalSign_form_utility.dart
+++ lib/Widget/evaluationForms/ultilities/form_utility/vitalSign_form_utility.dart
@@ -1,15 +1,18 @@
-import 'package:AbdoCare_Web/constants.dart';
+import 'package:AbdoCare_Web/form_criteria.dart';
 
 class VitalSignFormUtility {
   VitalSignFormUtility();
   bool getVitalSignFormCriteria(_bt, _pr, _rr, _systolic, _diastolic, _o2sat) {
     bool result = false;
-    if ((_bt >= Constant.btLowerCriteria && _bt <= Constant.btUpperCriteria) &&
-        (_pr >= Constant.prLowerCriteria && _pr <= Constant.prUpperCriteria) &&
-        (_rr >= Constant.rrLowerCriteria && _rr <= Constant.rrUpperCriteria) &&
-        (_systolic < Constant.systolicCriteria &&
-            _diastolic < Constant.diastolicCriteria) &&
-        _o2sat > Constant.oxygenCriteria) {
+    if ((_bt >= FormCriteria.BT_LOWER_CRITERIA &&
+            _bt <= FormCriteria.BT_UPPER_CRITERIA) &&
+        (_pr >= FormCriteria.PR_LOWER_CRITERIA &&
+            _pr <= FormCriteria.PR_UPPER_CRITERIA) &&
+        (_rr >= FormCriteria.RR_LOWER_CRITERIA &&
+            _rr <= FormCriteria.RR_UPPER_CRITERIA) &&
+        (_systolic < FormCriteria.SYSTOLIC_CRITERIA &&
+            _diastolic < FormCriteria.DIASTOLIC_CRITERIA) &&
+        _o2sat > FormCriteria.OXYGEN_CRITERIA) {
       result = false;
     } else {
       result = true;
