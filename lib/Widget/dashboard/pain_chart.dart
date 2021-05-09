12d7e0e35dfe74a96ea7eaf738c03537a306b5d4
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-09 23:18:21 +0700

    query pain data

--- lib/Widget/dashboard/pain_chart.dart
+++ lib/Widget/dashboard/pain_chart.dart
@@ -1,3 +1,4 @@
+import 'package:AbdoCare_Web/Widget/shared/progress_bar.dart';
 import 'package:AbdoCare_Web/services/interfaces/firebase_service_interface.dart';
 import 'package:AbdoCare_Web/services/service_locator.dart';
 import 'package:flutter/cupertino.dart';
@@ -16,67 +17,44 @@ class PainChart extends StatefulWidget {
 class _PainChartState extends State<PainChart> {
   final _firebaseService = locator<IFirebaseService>();
   List<_PainData> data = [];
-// _PainData('10/03/64\n02.00น', 9),
-  // _PainData('10/03/64\n06.00น', 9),
-  // _PainData('10/03/64\n10.00น', 8),
-  // _PainData('10/03/64\n14.00น', 8),
-  // _PainData('10/03/64\n18.00น', 8),
-  // _PainData('10/03/64\n22.00น', 8),
-  // _PainData('11/03/64\n02.00น', 7),
-  // _PainData('11/03/64\n06.00น', 9),
-  // _PainData('11/03/64\n10.00น', 9),
-  // _PainData('11/03/64\n14.00น', 7),
-  // _PainData('11/03/64\n18.00น', 6),
-  // _PainData('11/03/64\n22.00น', 6),
-  // _PainData('12/03/64\n02.00น', 6),
-  // _PainData('12/03/64\n06.00น', 6),
-  // _PainData('12/03/64\n10.00น', 7),
-  // _PainData('12/03/64\n14.00น', 6),
-  // _PainData('12/03/64\n18.00น', 5),
-  // _PainData('12/03/64\n22.00น', 5),
 
-  void initData() async {
-    var painData = await _firebaseService.getPainChart(hn: widget.hn);
-    painData.forEach((element) {
-      var painScore = element['formData']['pain'];
-      final formatter = DateFormat('dd/MM/yyyy');
-      String format = formatter.format(element['creation'].toDate());
-      String formTime = element['formTime'];
-      String date = '$format\n$formTime';
-      data.add(_PainData(date, painScore));
-    });
-
-    print('pain Data = $data');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initData();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          legend: Legend(isVisible: false),
-          tooltipBehavior: TooltipBehavior(enable: true),
-          series: <ChartSeries<_PainData, String>>[
-            LineSeries<_PainData, String>(
-                dataSource: data,
-                xValueMapper: (pain, _) => pain.painscore,
-                yValueMapper: (pain, _) => pain.day,
-                name: 'Pain score',
-                color: Colors.red,
-                dataLabelSettings: DataLabelSettings(isVisible: true))
-          ]),
-    );
+    return FutureBuilder<List<Map<String, dynamic>>>(
+        future: _firebaseService.getPainChart(hn: widget.hn),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return ProgressBar.circularProgressIndicator(context);
+          }
+          snapshot.data.forEach((element) {
+            var painScore = element['formData']['pain'];
+            final formatter = DateFormat('dd/MM/yyyy');
+            String format = formatter.format(element['creation'].toDate());
+            String formTime = element['formTime'];
+            String date = '$format\n$formTime';
+            data.add(_PainData(day: date, painscore: painScore));
+          });
+
+          return Container(
+            child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                legend: Legend(isVisible: false),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_PainData, String>>[
+                  LineSeries<_PainData, String>(
+                      dataSource: data,
+                      xValueMapper: (pain, _) => pain.day,
+                      yValueMapper: (pain, _) => pain.painscore,
+                      name: 'Pain score',
+                      color: Colors.red,
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+          );
+        });
   }
 }
 
 class _PainData {
-  _PainData(this.painscore, this.day);
-  final String painscore;
-  final double day;
+  _PainData({this.painscore, this.day});
+  final String day;
+  final double painscore;
 }
