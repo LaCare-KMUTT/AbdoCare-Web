a4cc0e22a581cc744c614e4b213252de1987f558
Thaweesak Saiwongse <Thaweesak_sai@hotmail.com>
2021-05-19 02:32:22 +0700

    config index.html

---
+++ web/firebase-config.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyDu8_HYCIsMBc8PkNs5oddKhjcJLFoNaVk",
+    authDomain: "abdocare-bdab0.firebaseapp.com",
+    databaseURL: "https://abdocare-bdab0.firebaseio.com",
+    projectId: "abdocare-bdab0",
+    storageBucket: "abdocare-bdab0.appspot.com",
+    messagingSenderId: "611491873650",
+    appId: "1:611491873650:web:7c3d372d1cdf8f314393eb",
+    measurementId: "G-Q85QT7BLYL"
+  };
