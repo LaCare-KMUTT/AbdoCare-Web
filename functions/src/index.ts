3daaf22bfe80ee39f8f82dd20d282320ecc5729a
Thaweesak <thaweesak_sai@hotmail.com>
2022-01-06 02:33:14 +0700

    Fix export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as admin from "firebase-admin";
 import { createUser } from "./modules/createUser";
 import { moveDocument } from "./modules/moveDocument";
+import { sendAppointmentNotification} from "./modules/sendAppointmentNotification";
 
 admin.initializeApp();
 exports.createUser = createUser;
 exports.moveDocument = moveDocument;
+exports.sendAppointmentNotification = sendAppointmentNotification;
